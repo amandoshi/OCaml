e8913a04e9f121e6fe54b367531899d4ba0918f6
Aman Doshi <amandoshi001@gmail.com>
2024-09-13 13:15:07 -0700

    Clean up code in table project

--- table/table.ml
+++ table/table.ml
@@ -3,22 +3,21 @@ open Stdio
 
 let col_width head body =
   let lengths l = List.map ~f:String.length l in
-  let accumulator acc l = List.map2_exn ~f:Int.max acc (lengths l) in
-  List.fold ~f:accumulator ~init:(lengths head) body
+  let combine acc l = List.map2_exn ~f:Int.max acc (lengths l) in
+  List.fold ~f:combine ~init:(lengths head) body
 
-let row_sep widths =
+let render_sep widths =
   let dashes w = String.make w '-' in
   "+-" ^ String.concat ~sep:"-+-" (List.map ~f:dashes widths) ^ "-+\n"
 
-let row_cells widths row =
-  let cell_str cell w = cell ^ (String.make (w - String.length cell) ' ') in
-  let row_spaced = List.map2_exn ~f:cell_str row widths in
-  "| " ^ String.concat ~sep:" | " row_spaced ^ " |\n"
+let render_row widths row =
+  let pad s width = s ^ (String.make (width - String.length s) ' ') in
+  "| " ^ String.concat ~sep:" | " (List.map2_exn ~f:pad row widths) ^ " |\n"
 
 let render_table head body =
   let widths = col_width head body in
-  let sep = row_sep widths in
-  let str_of_row = row_cells widths in
+  let sep = render_sep widths in
+  let str_of_row = render_row widths in
   let head_str = str_of_row head in
   let body_str = String.concat (List.map ~f:str_of_row body) in
   sep ^ head_str ^ sep ^ body_str ^ sep
